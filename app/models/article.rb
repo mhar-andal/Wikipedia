80a8a8ae2fab6321b6f18ed3c4c8df8f5cd24b5f
Dereck Miller <email": null>
2017-01-25 15:45:21 -0600

    1/25/2017 3:45PM create routes and update model associations

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,8 @@
 class Article < ApplicationRecord
+  has_many :sections, :bibliographies
+  belongs_to :user
+  has_many :revisions, as: :revisionable
+  has_many :notes
+  has_many :revisioned_sections, through: :sections, source: :revisions
+
 end

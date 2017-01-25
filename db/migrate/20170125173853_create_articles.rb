cdb3544e8c4bc1f7fb7c8041bfcc00489065769b
Mhar Andal <mharandal96@gmail.com>
2017-01-25 11:50:25 -0600

    Change publication_status from bool to string

--- db/migrate/20170125173853_create_articles.rb
+++ db/migrate/20170125173853_create_articles.rb
@@ -2,7 +2,7 @@ class CreateArticles < ActiveRecord::Migration[5.0]
   def change
     create_table :articles do |t|
       t.integer :author_id
-      t.boolean :publication_status
+      t.string :publication_status
       t.integer :cuurent_revision_id
 
       t.timestamps

778a115eb1d752dd104fffb1960b6799783d3ed8
Mhar Andal <email": null>
2017-01-26 15:16:16 -0600

    1/26/2017 3:15PM fixed responsive design for homepage

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,7 @@ class SessionsController < ApplicationController
 
   def create
     @user = User.find_by(username: user_deets[:username])
+    p @user
     if @user
       if @user.authenticate(user_deets[:password])
         login_user
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,5 @@
 class WelcomeController < ApplicationController
   def index
-    
+    @featured_article = Article.find((1..Article.all.count).to_a.sample)
   end
 end

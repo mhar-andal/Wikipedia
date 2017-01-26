1daee65ca6b7fe17e3e624ad4cbcb4ef2ad4260e
Dereck Miller <email": null>
2017-01-26 10:45:10 -0600

    1/26/2017 10:45AM Create show controller and view, helper methods in article and section models

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,4 +2,8 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.alphabetical
   end
+
+  def show
+    @article = Article.find(params[:id])
+  end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -10,6 +10,10 @@ class Article < ApplicationRecord
     self.joins("JOIN revisions ON articles.current_revision_id = revisions.id AND articles.publication_status = 'published'").order('revisions.title')
   end
 
+  def current_revision
+    Revision.find(self.current_revision_id)
+  end
+
   def newest_revision
     self.revisions[0] ? self.revisions[0] : nil
   end

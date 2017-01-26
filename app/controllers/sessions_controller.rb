3e3e374c8715119c04ffeb73069d695cad08d5b2
Mhar Andal <mharandal96@gmail.com>
2017-01-26 11:25:35 -0600

    1/26/2017 11:25AM create user auth

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,35 @@
+class SessionsController < ApplicationController
+  skip_before_filter  :verify_authenticity_token
+  include SessionsHelper
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.find_by(username: user_deets[:username])
+    if @user
+      if @user.authenticate(user_deets[:password])
+        login_user
+
+        redirect_to root_path
+      else
+        @user.errors.messages[:That] = ["combination of username and password can't be found"]
+        render :action => 'new'
+      end
+    else
+      @user = User.new
+      @user.errors.messages[:username] = ["doesn't exist"]
+      render :action => 'new'
+    end
+  end
+
+  def destroy
+    logout_user
+    redirect_to root_path
+  end
+
+private
+  def user_deets
+      params.require(:user).permit(:username, :password)
+  end
+end
